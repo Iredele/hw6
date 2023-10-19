306543c5b4887f0fff1365550447f2c9e0763120
TheOneAboveAll <iredeledayo@yahoo.com>
2023-10-19 02:41:55 -0700

    first commit

---
+++ software/hw6/obj/default/hello_world.d
@@ -0,0 +1,21 @@
+obj/default/hello_world.o: hello_world.c \
+ ../hw6_bsp//drivers/inc/altera_avalon_pio_regs.h \
+ ../hw6_bsp//HAL/inc/io.h ../hw6_bsp//HAL/inc/alt_types.h \
+ ../hw6_bsp//HAL/inc/sys/alt_irq.h ../hw6_bsp//HAL/inc/nios2.h \
+ ../hw6_bsp//HAL/inc/alt_types.h ../hw6_bsp/system.h ../hw6_bsp/linker.h
+
+../hw6_bsp//drivers/inc/altera_avalon_pio_regs.h:
+
+../hw6_bsp//HAL/inc/io.h:
+
+../hw6_bsp//HAL/inc/alt_types.h:
+
+../hw6_bsp//HAL/inc/sys/alt_irq.h:
+
+../hw6_bsp//HAL/inc/nios2.h:
+
+../hw6_bsp//HAL/inc/alt_types.h:
+
+../hw6_bsp/system.h:
+
+../hw6_bsp/linker.h:
---
+++ software/hw6_pt2/obj/default/hello_world.d
@@ -0,0 +1,22 @@
+obj/default/hello_world.o: hello_world.c \
+ ../hw6_pt2_bsp//drivers/inc/altera_avalon_pio_regs.h \
+ ../hw6_pt2_bsp//HAL/inc/io.h ../hw6_pt2_bsp//HAL/inc/alt_types.h \
+ ../hw6_pt2_bsp//HAL/inc/sys/alt_irq.h ../hw6_pt2_bsp//HAL/inc/nios2.h \
+ ../hw6_pt2_bsp//HAL/inc/alt_types.h ../hw6_pt2_bsp/system.h \
+ ../hw6_pt2_bsp/linker.h
+
+../hw6_pt2_bsp//drivers/inc/altera_avalon_pio_regs.h:
+
+../hw6_pt2_bsp//HAL/inc/io.h:
+
+../hw6_pt2_bsp//HAL/inc/alt_types.h:
+
+../hw6_pt2_bsp//HAL/inc/sys/alt_irq.h:
+
+../hw6_pt2_bsp//HAL/inc/nios2.h:
+
+../hw6_pt2_bsp//HAL/inc/alt_types.h:
+
+../hw6_pt2_bsp/system.h:
+
+../hw6_pt2_bsp/linker.h:
